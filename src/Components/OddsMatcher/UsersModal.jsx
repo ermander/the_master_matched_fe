205d6142bac6fc0d6da5cdc126700497b109a27e
Emanuele Bertuol <emanuelebertuol@gmail.com>
2020-11-25 14:19:38 +0100

    v

--- src/Components/OddsMatcher/UsersModal.jsx
+++ src/Components/OddsMatcher/UsersModal.jsx
@@ -82,7 +82,7 @@ class UsersModal extends Component {
     render() {
         return (
             <>
-                <Modal show={this.props.show} onHide={this.props.noShow}>
+                <Modal show={this.props.show} onHide={this.props.noShow} style={{maxWidth: "70vw", marginLeft: "15vw", marginRight: "15vw"}}>
                     <Modal.Header closeButton>
                         <Modal.Title>Intestatari Punta - Banca</Modal.Title>
                     </Modal.Header>
